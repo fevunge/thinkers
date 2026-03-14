052fa76b6358e732e729ee35cdbd89c49ddac63e
fevunge <developervunge@gmail.com>
2026-03-14 21:32:13 +0100

    fix: correct argument parsing and validation in get_args.c

--- philo/src/io/get_args.c
+++ philo/src/io/get_args.c
@@ -16,12 +16,11 @@ static int	get_argument(const char *arg)
 {
 	long	value;
 
-	/*if (!ft_s_is_digit(arg))
-		get_error(ARGUMENT_ERROR, ARG_TYPE_ERROR_MSG);*/
+	if (!ft_s_is_digit(arg))
+		get_error(ARGUMENT_ERROR, ARG_TYPE_ERROR_MSG);
 	value = ft_atoi(arg);
-	/*
-	if (value > INT_MAX || value < 0)
-		get_error(ARGUMENT_ERROR, ARG_TYPE_ERROR_MSG);*/
+	if (value > INT_MAX || value <= 0)
+		get_error(ARGUMENT_ERROR, ARG_TYPE_ERROR_MSG);
 	return ((int)value);
 }
 
@@ -32,10 +31,10 @@ t_args	get_args(int argc, const char *argv[])
 	if (((argc - 1) / 2) != 2)
 		get_error(ARGUMENT_ERROR, ARG_LEN_ERROR_MSG);
 	args.number_of_philos = get_argument(argv[1]);
-	args.number_of_philos = get_argument(argv[2]);
+	args.time_to_die = get_argument(argv[2]);
 	args.time_to_eat = get_argument(argv[3]);
 	args.time_to_sleep = get_argument(argv[4]);
-	if (argc == 5)
+	if (argc == 6)
 		args.times_must_eat = get_argument(argv[5]);
 	else
 		args.times_must_eat = -1;
