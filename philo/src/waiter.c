aa722909d6058f5cca0a33fe685141d994f74e97
fevunge <developervunge@gmail.com>
2026-03-16 04:57:20 +0100

    refactor waiter.c: rename eaten to has_eaten, switch to dinner locks, replace printf with get_log, set somebody_die flag

--- philo/src/waiter.c
+++ philo/src/waiter.c
@@ -6,7 +6,7 @@
 /*   By: fevunge <fevunge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 19:44:10 by fevunge           #+#    #+#             */
-/*   Updated: 2026/03/14 14:20:13 by fevunge          ###   ########.fr       */
+/*   Updated: 2026/03/15 20:57:34 by fevunge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_bool all_ate(t_philo *philos, int count)
 	while (i < count)
 	{
 		pthread_mutex_lock(philos->resources.meal_lock);
-		if (philos[i].eaten < philos[i].must_eat)
+		if (philos[i].has_eaten < philos[i].must_eat)
 			all_ate = FALSE;
 		pthread_mutex_unlock(philos->resources.meal_lock);
 		i++;
@@ -34,11 +34,11 @@ t_bool all_ate(t_philo *philos, int count)
 	return (FALSE);
 }
 
-void	*waiter_work(void *arg)
+void *waiter_work(void *arg)
 {
-	t_dinner	*dinner;
-	t_philo		*philos;
-	int			i;
+	t_dinner *dinner;
+	t_philo *philos;
+	int i;
 
 	dinner = (t_dinner *)arg;
 	philos = dinner->philos;
@@ -47,36 +47,31 @@ void	*waiter_work(void *arg)
 		i = 0;
 		while (i < dinner->args.number_of_philos)
 		{
-			pthread_mutex_lock(philos[i].resources.meal_lock);
+			pthread_mutex_lock(dinner->meal_lock);
 			if (philo_starved(philos[i]))
 			{
-				pthread_mutex_unlock(philos[i].resources.meal_lock);
-				pthread_mutex_lock(philos[i].resources.write_lock);
-				printf("%ld %d %s\n",
-					ft_time_now() - philos[i].born_at,
-					philos[i].id, DEATH_LOG);
+				pthread_mutex_unlock(dinner->meal_lock);
+				dinner->somebody_die = TRUE;
+				get_log(&philos[i], DEATH_LOG);
+				pthread_mutex_lock(dinner->write_lock);
+				
 				return (NULL);
 			}
-			pthread_mutex_unlock(philos[i].resources.meal_lock);
+			pthread_mutex_unlock(dinner->meal_lock);
 			i++;
 		}
 		if (all_ate(philos, dinner->args.number_of_philos))
-		{
-			pthread_mutex_lock(philos[0].resources.write_lock);
 			return (NULL);
-		}
 	}
 	return (NULL);
 }
 
-t_waiter *call_waiter(t_dinner *dinner)
+void	call_waiter(t_dinner *dinner)
 {
-	t_waiter *waiter;
+	pthread_t	waiter;
 
-	waiter = malloc(sizeof(t_waiter) * 1);
-	if (!waiter)
+	if (pthread_create(&waiter, NULL, &waiter_work, dinner) != 0)
 		finish_dinner(dinner);
-	if (pthread_create(&waiter->thread_id, NULL, &waiter_work, dinner) != 0)
-		finish_dinner(dinner);
-	return (waiter);
+	dinner->waiter = waiter;
+	return ;
 }
