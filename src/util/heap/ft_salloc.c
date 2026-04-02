548e1a02638614adec4aafc75c29fec438db0515
fevunge <developervunge@gmail.com>
2026-04-02 06:01:09 +0100

    feat: implement memory allocation function ft_salloc

---
+++ src/util/heap/ft_salloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_salloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fevunge <fevunge@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/15 11:49:13 by fevunge           #+#    #+#             */
+/*   Updated: 2026/03/16 07:58:31 by fevunge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*ft_salloc(size_t size, size_t len)
+{
+	void	*mem;
+
+	mem = malloc(size * len);
+	if (!mem)
+		get_error(MEMORY_ERROR, "Allocation error");
+	return (mem);
+}
