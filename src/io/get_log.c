baa51d94339ea47d5799d2d1ed4362cbb6a00e18
fevunge <developervunge@gmail.com>
2026-05-02 14:51:03 +0100

    chore: add source code implementation for input/output into the project

--- src/io/get_log.c
+++ src/io/get_log.c
@@ -1,31 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_log.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fevunge <fevunge@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/09 19:42:01 by fevunge           #+#    #+#             */
-/*   Updated: 2026/03/16 08:03:58 by fevunge          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "thinker.h"
 
-#include "philo.h"
-
-void	get_log(t_philo *philo, const char *log_message)
+void	get_log(t_thinker *thinker, const char *log_message)
 {
 	t_milisecond	age;
 
-	pthread_mutex_lock(philo->death_lock);
-	if (*philo->somebody_die)
+	pthread_mutex_lock(thinker->death_lock);
+	if (*thinker->somebody_die)
 	{
-		pthread_mutex_unlock(philo->death_lock);
+		pthread_mutex_unlock(thinker->death_lock);
 		return ;
 	}
-	pthread_mutex_lock(philo->write_lock);
-	age = ft_time_now() - philo->born_at;
-	printf("%ld %d %s\n", age, philo->id, log_message);
-	pthread_mutex_unlock(philo->write_lock);
-	pthread_mutex_unlock(philo->death_lock);
+	pthread_mutex_lock(thinker->write_lock);
+	age = ft_time_now() - thinker->born_at;
+	printf("%ld %d %s\n", age, thinker->id, log_message);
+	pthread_mutex_unlock(thinker->write_lock);
+	pthread_mutex_unlock(thinker->death_lock);
 	return ;
 }
